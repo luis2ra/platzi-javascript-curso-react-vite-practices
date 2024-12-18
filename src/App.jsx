d5e5bc0f403887e32c1eba2b7a6963e0ded3dae2
Luis Altuve <laltuve@ext.sebbusiness.com>
2024-12-18 15:36:59 -0500

    initial base project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="bg-blue-100">
+        Hola Mundo con React, Vite y TailwindCSS!!!
+      </p>
+    </>
+  )
+}
+
+export default App
